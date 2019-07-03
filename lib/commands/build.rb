48051b73ebbea28d47909fb5593779afb9123ba6
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2019-07-03 17:28:05 +0100

    Also cleanup after setup for individual apps
    This ensures we cleanup after making specific services.

--- lib/commands/build.rb
+++ lib/commands/build.rb
@@ -5,5 +5,7 @@ class Commands::Build < Commands::Base
   def call
     check_service_exists
     system.call("make", "-f", "#{config_directory}/Makefile", service)
+    Commands::Compose.new.call("stop")
+    Commands::Prune.new.call
   end
 end
--- spec/commands/build_spec.rb
+++ spec/commands/build_spec.rb
@@ -5,15 +5,26 @@ require_relative "../../lib/errors/unknown_service"
 describe Commands::Build do
   let(:config_directory) { "spec/fixtures" }
   let(:service) { nil }
-  let(:system) { double(:system) }
+  let(:system) { double(:system, call: true) }
 
   subject { described_class.new(service, config_directory, system) }
 
   context "when a service exists" do
     let(:service) { "example-service" }
+    let(:compose_command) { instance_double Commands::Compose, call: true }
+    let(:prune_command) { instance_double Commands::Prune, call: true }
 
-    it "should run docker compose when a service exists" do
+    before { expect(Commands::Compose).to receive(:new).and_return(compose_command) }
+    before { expect(Commands::Prune).to receive(:new).and_return(prune_command) }
+
+    it "should run make for the service" do
       expect(system).to receive(:call).with("make", "-f", "#{config_directory}/Makefile", "example-service")
+      subject.call
+    end
+
+    it "should cleanup after itself" do
+      expect(compose_command).to receive(:call).with("stop")
+      expect(prune_command).to receive(:call)
       subject.call
     end
   end
