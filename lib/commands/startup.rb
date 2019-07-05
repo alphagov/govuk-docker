23daf02ec6bd7ea64d07c4a7f148e3a2e2f2afb2
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2019-07-05 09:41:34 +0100

    Print when the application becomes live
    This means you see a message like
    
    ```
    Application is available at: http://government-frontend.dev.gov.uk
    ```
    
    when the app starts.

--- lib/commands/startup.rb
+++ lib/commands/startup.rb
@@ -1,3 +1,7 @@
+require 'colorize'
+require 'net/http'
+require 'timeout'
+
 require_relative './base'
 require_relative './run'
 
@@ -5,8 +9,67 @@ class Commands::Startup < Commands::Base
   def call(variation = nil)
     stack = variation ? "app-#{variation}" : "app"
 
+    print_live_hostname
+
     Commands::Run
       .new(config_directory: config_directory, service: service, stack: stack, verbose: verbose)
       .call
+  end
+
+private
+
+  def print_live_hostname
+    return unless hostname
+
+    Thread.new do
+      url = "http://#{hostname}"
+
+      Timeout::timeout(30) do
+        wait_until_can_visit?(url)
+
+        puts
+        puts "Application is available at: #{url}".blue
+        puts "\n\r"
+      end
+    rescue Timeout::Error
+      puts
+      puts "Warning: Unable to communicate with application within 30 seconds.".red
+      puts "\n\r"
+    end
+  end
+
+  def possible_hostnames
+    path = File.join(config_directory, "services", "nginx-proxy", "docker-compose.yml")
+    docker_compose = YAML.load_file(path)
+    docker_compose["services"]["nginx-proxy-app"]["networks"]["default"]["aliases"]
+  end
+
+  def find_hostname
+    search_hostname = "#{service.tr('_-', '')}.dev.gov.uk"
+    possible_hostnames.each do |hostname|
+      return hostname if hostname.tr('_-', '') == search_hostname
+    end
+
+    nil
+  end
+
+  def hostname
+    @hostname ||= find_hostname
+  end
+
+  def wait_until_can_visit?(url)
+    loop do
+      sleep(1)
+      break if can_visit?(url)
+    end
+  end
+
+  def can_visit?(url)
+    case Net::HTTP.get_response(URI(url))
+    when Net::HTTPSuccess, Net::HTTPRedirection then
+      true
+    else
+      false
+    end
   end
 end
--- spec/commands/startup_spec.rb
+++ spec/commands/startup_spec.rb
@@ -2,10 +2,18 @@ require "spec_helper"
 require_relative "../../lib/commands/startup"
 
 describe Commands::Startup do
-  subject { described_class.new }
+  let(:config_directory) { "spec/fixtures" }
+
+  subject { described_class.new(config_directory: config_directory, service: "example-service") }
 
   let(:run_double) { instance_double(Commands::Run) }
   before { allow(run_double).to receive(:call) }
+
+  before do
+    allow(Thread).to receive(:new).and_yield # to run the thread in the current context
+    allow(subject).to receive(:wait_until_can_visit?).and_return(true)
+    allow(subject).to receive(:puts)
+  end
 
   context "without a variation" do
     it "calls `Run` in the correct stack" do
@@ -19,5 +27,12 @@ describe Commands::Startup do
       expect(Commands::Run).to receive(:new).with(a_hash_including(stack: "app-e2e")).and_return(run_double)
       subject.call("e2e")
     end
+  end
+
+  it "prints the URL of the app" do
+    expect(Commands::Run).to receive(:new).with(a_hash_including(stack: "app")).and_return(run_double)
+    expect(subject).to receive(:wait_until_can_visit?).and_return(true)
+    expect(subject).to receive(:puts).with("\e[0;34;49mApplication is available at: http://example_service.dev.gov.uk\e[0m")
+    subject.call
   end
 end
