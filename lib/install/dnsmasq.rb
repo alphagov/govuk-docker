c8f13c6742ef132d053eab1b724172fd137b221b
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2019-07-05 12:06:51 +0100

    Reduce indentation in dnsmasq install command

--- lib/install/dnsmasq.rb
+++ lib/install/dnsmasq.rb
@@ -1,89 +1,87 @@
 require_relative "./base"
 require_relative "../doctor/dnsmasq"
 
-module Install
-  class Dnsmasq < Base
-    def call
-      return unless check_continue
+class Install::Dnsmasq < Install::Base
+  def call
+    return unless check_continue
 
-      install_dnsmasq
-      configure_etc_resolver_devgovuk
-      configure_usr_local_etc_dnsmasq
-      configure_usr_local_etc_dnsmasq_developmentconf
-      restart_dnsmasq
-      puts "✅ Dnsmasq installation and configuration complete!"
-    end
+    install_dnsmasq
+    configure_etc_resolver_devgovuk
+    configure_usr_local_etc_dnsmasq
+    configure_usr_local_etc_dnsmasq_developmentconf
+    restart_dnsmasq
+    puts "✅ Dnsmasq installation and configuration complete!"
+  end
 
-  private
+private
 
-    def check_continue
-      puts "Any local changes in these files may get overwriten by this script:"
-      puts "- /etc/resolver/dev.gov.uk"
-      puts "- /usr/local/etc/dnsmasq.conf"
-      puts "- /usr/local/etc/dnsmasq.d/development.conf"
+  def check_continue
+    puts "Any local changes in these files may get overwriten by this script:"
+    puts "- /etc/resolver/dev.gov.uk"
+    puts "- /usr/local/etc/dnsmasq.conf"
+    puts "- /usr/local/etc/dnsmasq.d/development.conf"
+    puts
+
+    unless %x[uname -a].include?("Darwin")
+      puts "This script is designed to run on MacOS."
       puts
-
-      unless %x[uname -a].include?("Darwin")
-        puts "This script is designed to run on MacOS."
-        puts
-      end
-
-      shell.yes?("Are you sure you want to continue?")
     end
 
-    def install_dnsmasq
-      return if Doctor::Dnsmasq.new.installed?
+    shell.yes?("Are you sure you want to continue?")
+  end
 
-      puts "⏳ Installing dnsmasq"
-      system("brew install dnsmasq")
-    end
+  def install_dnsmasq
+    return if Doctor::Dnsmasq.new.installed?
 
-    def configure_etc_resolver_devgovuk
-      write_file(
-        "/etc/resolver/dev.gov.uk",
-        "nameserver 127.0.0.1"
-      )
-    end
+    puts "⏳ Installing dnsmasq"
+    system("brew install dnsmasq")
+  end
 
-    def configure_usr_local_etc_dnsmasq
-      append_file(
-        "/usr/local/etc/dnsmasq.conf",
-        "conf-dir=/usr/local/etc/dnsmasq.d,*.conf"
-      )
-    end
+  def configure_etc_resolver_devgovuk
+    write_file(
+      "/etc/resolver/dev.gov.uk",
+      "nameserver 127.0.0.1"
+    )
+  end
 
-    def configure_usr_local_etc_dnsmasq_developmentconf
-      write_file(
-        "/usr/local/etc/dnsmasq.d/development.conf",
-        "address=/dev.gov.uk/127.0.0.1"
-      )
-    end
+  def configure_usr_local_etc_dnsmasq
+    append_file(
+      "/usr/local/etc/dnsmasq.conf",
+      "conf-dir=/usr/local/etc/dnsmasq.d,*.conf"
+    )
+  end
 
-    def restart_dnsmasq
-      puts "♻️  Restarting dnsmasq, you may need to enter your root password..."
-      system("sudo brew services restart dnsmasq")
-    end
+  def configure_usr_local_etc_dnsmasq_developmentconf
+    write_file(
+      "/usr/local/etc/dnsmasq.d/development.conf",
+      "address=/dev.gov.uk/127.0.0.1"
+    )
+  end
 
-    def file_configured?(path, contents)
-      File.read(path).include?(contents)
-    rescue Errno::ENOENT
-      false
-    end
+  def restart_dnsmasq
+    puts "♻️  Restarting dnsmasq, you may need to enter your root password..."
+    system("sudo brew services restart dnsmasq")
+  end
 
-    def write_file(path, contents)
-      return if file_configured?(path, contents)
+  def file_configured?(path, contents)
+    File.read(path).include?(contents)
+  rescue Errno::ENOENT
+    false
+  end
 
-      puts "⏳ Writing #{path}"
-      File.write(path, "#{contents}\n")
-    end
+  def write_file(path, contents)
+    return if file_configured?(path, contents)
 
-    def append_file(path, contents)
-      return if file_configured?(path, contents)
+    puts "⏳ Writing #{path}"
+    File.write(path, "#{contents}\n")
+  end
 
-      puts "⏳ Appending #{path}"
-      File.open(path, 'a') do |file|
-        file.write("\n#{contents}\n")
-      end
+  def append_file(path, contents)
+    return if file_configured?(path, contents)
+
+    puts "⏳ Appending #{path}"
+    File.open(path, 'a') do |file|
+      file.write("\n#{contents}\n")
     end
   end
 end
