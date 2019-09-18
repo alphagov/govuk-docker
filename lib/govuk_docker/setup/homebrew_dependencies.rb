c68ab0cdbdeece0416e7fead9a99ee2facaee22a
Issy Long <me@issyl0.co.uk>
2019-09-18 09:55:56 +0100

    Don't ask the user to continue when installing dependencies
    - This is a pre-requisite step to any of this working, and if you run a
      command you expect it to do the thing you ask of it (ie, setup your
      environment). :-)

--- lib/govuk_docker/setup/homebrew_dependencies.rb
+++ lib/govuk_docker/setup/homebrew_dependencies.rb
@@ -3,15 +3,7 @@ require_relative "../doctor/checkup"
 
 class GovukDocker::Setup::HomebrewDependencies < GovukDocker::Setup::Base
   def call
-    return unless check_continue
-
+    puts "Installing dependencies via Homebrew..."
     system("brew bundle --file=#{GovukDocker::Paths.govuk_docker_dir}/Brewfile")
-  end
-
-private
-
-  def check_continue
-    puts "This will install all the dependencies via Homebrew."
-    shell.yes?("Are you sure you want to continue?")
   end
 end
--- spec/setup/homebrew_dependencies_spec.rb
+++ spec/setup/homebrew_dependencies_spec.rb
@@ -10,25 +10,14 @@ describe GovukDocker::Setup::HomebrewDependencies do
     allow(File).to receive(:read).and_call_original
   end
 
-  context "disallowing the script to continue" do
-    it "shouldn't do anything" do
-      expect(subject).to receive(:puts).with(/This will install all the dependencies via Homebrew./)
-      expect(shell_double).to receive(:yes?).and_return(false)
-      expect(GovukDocker::Doctor::Checkup).to_not receive(:new)
-      expect(File).to_not receive(:read)
-      expect(subject).to_not receive(:system)
-      subject.call
-    end
-  end
-
-  context "allowing the script to continue" do
+  context "installing dependencies" do
     before do
-      allow(shell_double).to receive(:yes?).and_return(true)
       allow(subject).to receive(:system).with("brew bundle --file=#{GovukDocker::Paths.govuk_docker_dir}/Brewfile")
     end
 
     it "installs all the dependencies with brew" do
-      expect(subject).to receive(:system).with("brew bundle --file=#{ENV["HOME"]}/govuk/govuk-docker/Brewfile")
+      expect(subject).to receive(:puts).with("Installing dependencies via Homebrew...")
+      expect(subject).to receive(:system).with("brew bundle --file=#{ENV['HOME']}/govuk/govuk-docker/Brewfile")
       subject.call
     end
   end
