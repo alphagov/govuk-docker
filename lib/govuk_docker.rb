139b35037ae5bbf67ef010a251ddab642e8adf9d
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2019-11-27 17:44:50 +0000

    Rewrite setup script to be a single file
    Previously we had a Ruby script to setup GOV.UK Docker, which was hard
    to understand because the setup spanned multiple files and methods.
    
    This rewrites the setup into a single script. A couple of features of
    the original script haven't been carried over:
    
       - Since all the changes are necessary and isolated, we don't ask
    before doing them
       - We only expect people to run setup once in the repo, so no more
    repo/pull stuff
    
    This also adds an extra command to ensure the system resolver picks up
    the new config in /etc/resolver, which has been a problem for some.

--- lib/govuk_docker.rb
+++ lib/govuk_docker.rb
@@ -1,9 +1,5 @@
 module GovukDocker; end
 
 require "govuk_docker/paths"
-require "govuk_docker/setup/base"
-require "govuk_docker/setup/dnsmasq"
-require "govuk_docker/setup/homebrew_dependencies"
-require "govuk_docker/setup/repo"
 require "govuk_docker/doctor/doctor"
 require "govuk_docker/doctor/checkup"
