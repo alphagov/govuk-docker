c2bf35f524a2d32169d25e903b3bc14e06d31ec7
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2021-07-16 15:22:53 +0100

    Rename variables to be more descriptive of what they do

--- spec/integration/make/dependencies_spec.rb
+++ spec/integration/make/dependencies_spec.rb
@@ -3,19 +3,19 @@ require "spec_helper"
 RSpec.describe "Make dependencies" do
   ProjectsHelper.all_projects.each do |project_name|
     it "mirrors docker-compose.yml for #{project_name}" do
-      expect(compose_dependencies(project_name))
-        .to match_array(makefile_dependencies(project_name))
+      expect(app_dependencies_in_compose_file(project_name))
+        .to match_array(app_dependencies_in_makefile(project_name))
     end
   end
 
-  def compose_dependencies(project_name)
+  def app_dependencies_in_compose_file(project_name)
     project_stacks = ComposeHelper.services(project_name).values
     dependencies = project_stacks.flat_map { |s| s["depends_on"].to_a }
     dependencies = compose_remove_stack_from_service_name(dependencies)
     (dependencies & ProjectsHelper.all_projects) - [project_name]
   end
 
-  def makefile_dependencies(project_name)
+  def app_dependencies_in_makefile(project_name)
     MakefileHelper.dependencies(project_name) &
       compose_remove_stack_from_service_name(ComposeHelper.app_services.keys)
   end
