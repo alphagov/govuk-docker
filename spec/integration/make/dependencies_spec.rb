33844b28b0be22ea1db6b002fa3fb99f0c177e19
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2021-07-19 09:24:47 +0100

    Move 'dependencies that don't run' logic into methods
    This makes the top-level method easier & quicker to read.
    Unfortunately the array of apps is not accessible inside the
    methods unless I turn it into a constant, and then Rubocop
    complains, so I've added a lint override as I think this is still
    a neater separation of concerns than to bury the array inside
    its own method at the bottom of the file.

--- spec/integration/make/dependencies_spec.rb
+++ spec/integration/make/dependencies_spec.rb
@@ -1,13 +1,15 @@
 require "spec_helper"
 
 RSpec.describe "Make dependencies" do
+  # rubocop:disable Lint/ConstantDefinitionInBlock
   # Some dependencies are only needed by the dependant app for their static files.
-  dependencies_that_do_not_need_to_be_running = %w[govuk-content-schemas]
+  DEPENDENCIES_THAT_DO_NOT_NEED_TO_BE_RUNNING = %w[govuk-content-schemas].freeze
+  # rubocop:enable Lint/ConstantDefinitionInBlock
 
   ProjectsHelper.all_projects.each do |project_name|
     it "mirrors docker-compose.yml for #{project_name}" do
-      expect(app_dependencies_in_compose_file(project_name) - dependencies_that_do_not_need_to_be_running)
-        .to match_array(app_dependencies_in_makefile(project_name) - dependencies_that_do_not_need_to_be_running)
+      expect(app_dependencies_in_compose_file(project_name))
+        .to match_array(app_dependencies_in_makefile(project_name))
     end
   end
 
@@ -15,12 +17,14 @@ RSpec.describe "Make dependencies" do
     project_stacks = ComposeHelper.services(project_name).values
     dependencies = project_stacks.flat_map { |s| s["depends_on"].to_a }
     dependencies = compose_remove_stack_from_service_name(dependencies)
-    (dependencies & ProjectsHelper.all_projects) - [project_name]
+    app_dependencies = (dependencies & ProjectsHelper.all_projects) - [project_name]
+    app_dependencies - DEPENDENCIES_THAT_DO_NOT_NEED_TO_BE_RUNNING
   end
 
   def app_dependencies_in_makefile(project_name)
-    MakefileHelper.dependencies(project_name) &
+    app_dependencies = MakefileHelper.dependencies(project_name) &
       compose_remove_stack_from_service_name(ComposeHelper.app_services.keys)
+    app_dependencies - DEPENDENCIES_THAT_DO_NOT_NEED_TO_BE_RUNNING
   end
 
   def compose_remove_stack_from_service_name(dependencies)
